fc3ea89122507eb6368a679409c5ae04bf398155
Floran Mauguin <fmauguin.pro@gmail.com>
2022-08-29 15:57:25 +0200

    error free ok

--- srcs/floran/bvh/bvh_construction.c
+++ srcs/floran/bvh/bvh_construction.c
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 11:01:38 by amahla            #+#    #+#             */
-/*   Updated: 2022/08/29 15:42:09 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/08/29 15:57:18 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 #include "utils.h"
 #include "minirt.h"
 
-void	make_leaf(t_list *vols, t_bvh **root, unsigned int begin)
+void	make_leaf(t_list *vols, t_bvh **root, unsigned int begin, unsigned int end)
 {
 	t_vol	*vol;
 
 	vols = ft_lst_at(vols, begin);
 	vol = (t_vol *)(vols->content);
 	(*root)->left = btree_create_node(vol->box, vol);
-	if (vols->next)
+	if (vols->next && end != begin)
 	{
 		vol = (t_vol *)(vols->next->content);
 		(*root)->right = btree_create_node(vol->box, vol);
@@ -37,14 +37,13 @@ t_bvh	*build_node(t_list *vols, t_bvh **root, unsigned int begin, unsigned int e
 	*root = btree_create_node(box, NULL);
 	if (end - begin <= 1)
 	{
-		make_leaf(vols, root, begin);
+		make_leaf(vols, root, begin, end);
 		return (*root);
 	}
 	m = sort_vols(&vols, &box, begin, end);
 	ft_printf("\nm = %u\n", m);
 	(*root)->left = build_node(vols, &(*root)->left, begin, m - 1);
 	ft_printf("\nleft over\n");
-	
 	(*root)->right = build_node(vols, &(*root)->right, m, end);
 	return (*root);
 }
