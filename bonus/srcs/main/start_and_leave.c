e12f9989262a2d6f3d9823db6b067ea4c8925b6a
Floran Mauguin <fmauguin.pro@gmail.com>
2022-09-09 23:01:48 +0200

    free vols &planes

--- bonus/srcs/main/start_and_leave.c
+++ bonus/srcs/main/start_and_leave.c
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:25:50 by amahla            #+#    #+#             */
-/*   Updated: 2022/09/09 22:44:33 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/09/09 23:00:33 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,24 @@
 #include "utils.h"
 #include "bvh.h"
 
+static void	free_vols(t_vol *vol)
+{
+	if (vol->tex && vol->data)
+		mlx_destroy_image(vol->data->mlx_ptr, vol->tex->mlx_img);
+	if (vol->bump && vol->data)
+		mlx_destroy_image(vol->data->mlx_ptr, vol->bump->mlx_img);
+	free(vol);
+}
+
+static void	free_planes(t_plane *pl)
+{
+	if (pl->tex && pl->data)
+		mlx_destroy_image(pl->data->mlx_ptr, pl->tex->mlx_img);
+	if (pl->bump && pl->data)
+		mlx_destroy_image(pl->data->mlx_ptr, pl->bump->mlx_img);
+	free(pl);
+}
+
 void	init_scene(t_scene *scene)
 {
 	scene->vols = NULL;
@@ -22,6 +40,7 @@ void	init_scene(t_scene *scene)
 	scene->cameras = NULL;
 	scene->ambient = NULL;
 	scene->planes = NULL;
+	scene->line_gnl = NULL;
 	scene->bvh = NULL;
 	scene->cam = NULL;
 	scene->resolut.is_set = false;
@@ -30,10 +49,13 @@ void	init_scene(t_scene *scene)
 void	leave_rt(t_scene *scene)
 {
 	ft_lstclear(&scene->lights, &free);
-	ft_lstclear(&scene->vols, &free);
-	ft_lstclear(&scene->planes, &free);
+	ft_lstclear(&scene->vols, &free_vols);
+	ft_lstclear(&scene->planes, &free_planes);
 	ft_lstclear(&scene->cameras, &free);
 	if (scene->ambient)
 		free(scene->ambient);
-	btree_remove_infix(&scene->bvh, &free);
+	if (scene->line_gnl)
+		free(scene->ambient);
+	if (scene->bvh)
+		btree_remove_infix(&scene->bvh, &free);
 }
