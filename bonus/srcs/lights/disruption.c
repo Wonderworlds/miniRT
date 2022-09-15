06e448d1ba33d7ce8f1ecd5e1da620f3c28dce75
Ammah <amir.mahla@icloud.com>
2022-09-16 01:20:24 +0200

    disruption perfection

--- bonus/srcs/lights/disruption.c
+++ bonus/srcs/lights/disruption.c
@@ -6,7 +6,7 @@
 /*   By: amahla <amahla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 17:47:55 by amahla            #+#    #+#             */
-/*   Updated: 2022/09/16 00:42:59 by amahla           ###   ########.fr       */
+/*   Updated: 2022/09/16 01:19:41 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,24 @@
 #include "utils.h"
 #include <math.h>
 
-void	checkerboard_sphere(t_couplef uv, t_hit *hit)
+void	checkerboard(t_couplef uv, t_hit *hit)
 {
 	float	u2;
 	float	v2;
 	int		res;
+	int		coeff;
 
-	u2 = floorf(uv.x * 14);
-	v2 = floorf(uv.y * 14);
+
+	if (hit->vol_type == TRIANGLE)
+		coeff = 4;
+	else if (hit->vol_type == PLANE)
+		coeff = 1;
+	else if (hit->vol_type == SPHERE)
+		coeff = 14;
+	else
+		coeff = 12;
+	u2 = floorf(uv.x * coeff);
+	v2 = floorf(uv.y * coeff);
 	res = u2 + v2;
 	if (res % 2)
 	{
@@ -43,25 +53,11 @@ void	get_uvw_sp(t_hit *hit, t_vol *sp, t_couplef *uv)
 	uv->y = 0.5 - asinf(on_sp.y) / M_PI;
 }
 
-void	checkerboard(t_pos on_vol, t_hit *hit)
+void	get_uvw_pl(t_hit *hit, t_plane *pl, t_couplef *uv)
 {
-	float sines;
-
-	sines = sinf(10 * on_vol.x) * sinf(10 * on_vol.y)
-		* sinf(10 * on_vol.z);
-	if (sines < 0)
-	{
-		hit->col.r = 0;
-		hit->col.g = 0;
-		hit->col.b = 0;
-	}
-}
-
-void	get_uvw_pl(t_hit *hit, t_plane *pl, t_pos *on_pl)
-{
+	t_pos	on_pl;
 	t_pos	u;
 	t_pos	v;
-	t_pos	w;
 
 	cross_product(hit->normal, gen_vec(1, 0, 0), &u);
 	if (u.x == 0 && u.y == 0 && u.z == 0)
@@ -69,14 +65,10 @@ void	get_uvw_pl(t_hit *hit, t_plane *pl, t_pos *on_pl)
 	unit_vector(&u);
 	cross_product(hit->normal, u, &v);
 	unit_vector(&v);
-	cross_product(v, u, &w);
-	unit_vector(&w);
-	vector_sub(hit->pos, pl->pos, on_pl);
-	on_pl->x = dot_product(v, *on_pl);
-	on_pl->z = dot_product(u, *on_pl);
-	on_pl->y = dot_product(hit->normal, *on_pl);
+	vector_sub(hit->pos, pl->pos, &on_pl);
+	uv->x = dot_product(v, on_pl);
+	uv->y = dot_product(u, on_pl);
 }
-
 
 void	get_uvw_cy(t_hit *hit, t_vol *cy, t_couplef *uv)
 {
@@ -84,7 +76,6 @@ void	get_uvw_cy(t_hit *hit, t_vol *cy, t_couplef *uv)
 	t_pos	proj;
 	t_pos	u;
 	t_pos	v;
-	float	add;
 
 	cross_product(cy->vec3, gen_vec(1, 0, 0), &u);
 	if (u.x == 0 && u.y == 0 && u.z == 0)
@@ -96,24 +87,14 @@ void	get_uvw_cy(t_hit *hit, t_vol *cy, t_couplef *uv)
 	proj.x = dot_product(v, on_cy);
 	proj.y = dot_product(cy->vec3, on_cy);
 	proj.z = dot_product(u, on_cy);
-	if (proj.y < 0)
-		proj.y = 0;
 	float theta = atan2f(proj.x, proj.z);
     float rawU = theta / (2 * M_PI);
-	if (proj.x == 0 && proj.z == 0)
-		add = 0;
-	else
-		add = vector_norm(gen_vec(proj.x, 0, proj.z));
-	if (proj.y >= cy->h)
-		proj.y = cy->h + cy->d - add;
-	else
-		proj.y += add;
 	proj.y /= (cy->h + cy->d);
 	uv->x = 1 - (rawU + 0.5);
 	uv->y = fmodf(fmodf(proj.y, 1) + 1, 1);
 }
 
-void	get_uvw_tr(t_hit *hit, t_vol *tr, t_pos *on_tr)
+void	get_uvw_tr(t_hit *hit, t_vol *tr, t_couplef *uv)
 {
 	t_pos	on_pl;
 	t_pos	u;
@@ -126,9 +107,8 @@ void	get_uvw_tr(t_hit *hit, t_vol *tr, t_pos *on_tr)
 	cross_product(hit->normal, u, &v);
 	unit_vector(&v);
 	vector_sub(hit->pos, tr->tr[0], &on_pl);
-	on_tr->x = dot_product(v, on_pl);
-	on_tr->y = dot_product(u, on_pl);
-	on_tr->z = dot_product(hit->normal, on_pl);
+	uv->x = dot_product(v, on_pl);
+	uv->y = dot_product(u, on_pl);
 }
 
 void	get_uvw_co(t_hit *hit, t_vol *co, t_couplef *uv)
@@ -137,7 +117,6 @@ void	get_uvw_co(t_hit *hit, t_vol *co, t_couplef *uv)
 	t_pos	proj;
 	t_pos	u;
 	t_pos	v;
-	float	add;
 
 	cross_product(co->vec3, gen_vec(1, 0, 0), &u);
 	if (u.x == 0 && u.y == 0 && u.z == 0)
@@ -151,14 +130,6 @@ void	get_uvw_co(t_hit *hit, t_vol *co, t_couplef *uv)
 	proj.z = dot_product(u, on_co);
 	float theta = atan2f(proj.x, proj.z);
     float rawU = theta / (2 * M_PI);
-	if (proj.x == 0 && proj.z == 0)
-		add = 0;
-	else
-		add = vector_norm(gen_vec(proj.x, 0, proj.z));
-	if (proj.y == 0)
-		proj.y += add;
-	else
-		proj.y += co->d / 2;
 	proj.y /= (co->h + (co->d / 2));
 	uv->x = 1 - (rawU + 0.5);
 	uv->y = fmodf(fmodf(proj.y, 1) + 1, 1);
@@ -166,7 +137,6 @@ void	get_uvw_co(t_hit *hit, t_vol *co, t_couplef *uv)
 
 void	do_disruption2(t_disruption disruption, t_hit *hit)
 {
-	t_pos		on_vol;
 	t_couplef	uv;
 
 	if (disruption != NONE)
@@ -178,14 +148,11 @@ void	do_disruption2(t_disruption disruption, t_hit *hit)
 		else if (hit->vol_type == CYLINDER)
 			get_uvw_cy(hit, hit->vol, &uv);
 		else if (hit->vol_type == PLANE)
-			get_uvw_pl(hit, hit->vol, &on_vol);
+			get_uvw_pl(hit, hit->vol, &uv);
 		else
-			get_uvw_tr(hit, hit->vol, &on_vol);
-		if (disruption == CHECKERBOARD && (hit->vol_type == SPHERE
-				|| hit->vol_type == CONE || hit->vol_type == CYLINDER))
-			checkerboard_sphere(uv, hit);
-		else if (disruption == CHECKERBOARD)
-			checkerboard(on_vol, hit);
+			get_uvw_tr(hit, hit->vol, &uv);
+		if (disruption == CHECKERBOARD)
+			checkerboard(uv, hit);
 	}
 }
 
